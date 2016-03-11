e156f117713da9a416a41fe712b303db5762ad77
kaylaboch <boch.kayla57@gmail.com>
2016-03-11 17:00:23 -0500

    body sec

--- js/main.js
+++ js/main.js
@@ -2,5 +2,10 @@ var $newH1 = $('<h1>');
 var $belowveggies = $('<ul>');
 var $aboveH1 = $('<h1>');
 var $aboveveggies = $('<ul>');
+
 $newH1.html('Below ground veggies');
 $('body').prepend($newH1);
+
+$('body').append($belowveggies);
+$aboveH1.html('Above ground veggies');
+$('body').append($aboveH1);
